f7729c7d69c3f39632f8e8f358ead98ffa4e641c
altstudiya <altstudiya@inbox.ru>
2026-05-09 13:41:12 +0300

    Create main page

---
+++ app/page.js
@@ -0,0 +1,15 @@
+'use client';
+
+import HeroSection from '@/components/HeroSection';
+import OfferSection from '@/components/OfferSection';
+import ContactFormSection from '@/components/ContactFormSection';
+
+export default function HomePage() {
+  return (
+    <main>
+      <HeroSection />
+      <OfferSection />
+      <ContactFormSection />
+    </main>
+  );
+}
